8ec47c780ed721cc25c94fd0e60cfdcfa9b3adfc
Eugene Korenevsky <kara-91@tut.by>
2021-01-28 11:09:36 +0300

    feat: add exception

---
+++ src/main/java/com/example/hostel1/servicies/exceptions/RoomIsExistException.java
@@ -0,0 +1,7 @@
+package com.example.hostel1.servicies.exceptions;
+
+public class RoomIsExistException extends Exception {
+    public RoomIsExistException(String message) {
+        super(message);
+    }
+}
