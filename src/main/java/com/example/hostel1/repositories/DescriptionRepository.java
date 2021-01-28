bfe8f865832eea1f7fd3d9cb121a32e11c2b6aee
Eugene Korenevsky <kara-91@tut.by>
2021-01-28 11:27:03 +0300

    feat: add new methods

--- src/main/java/com/example/hostel1/repositories/DescriptionRepository.java
+++ src/main/java/com/example/hostel1/repositories/DescriptionRepository.java
@@ -2,6 +2,9 @@ package com.example.hostel1.repositories;
 
 import com.example.hostel1.entities.Description;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DescriptionRepository extends CrudRepository<Description,Long> {
+    public Description findByDescription(String description);
 }
